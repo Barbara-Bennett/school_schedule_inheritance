4e7c8d8b9526a6072f83fc7aec9dc102c5c40f06
Barbara <barbaraelisabethb@gmail.com>
2023-11-20 21:51:01 -0500

    Updates

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,17 @@
 from .student import Student
 
 # add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, 
+                 gets_transportation=False):
+        
+        super().__init__(name, grade, classes)
+        self.get_transportation = gets_transportation
+
+    def summary(self):
+        first_line = super().summary()
+        if self.get_transportation == True:
+            second_line = f"{self.name} has {self.get_transportation()}"
+            return first_line + second_line
+        else:
+            return first_line
